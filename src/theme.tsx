52f33107767f7793ec0fb45e5fcb51340c27c5e8
Frank Becker <fb@mooflu.com>
2025-08-13 16:33:16 -0700

    Avoid white color flash. Hide body until react theme is applied.

--- src/theme.tsx
+++ src/theme.tsx
@@ -57,6 +57,7 @@ const useTheme = () => {
                         body: {
                             width: '100%',
                             height: '100%',
+                            display: 'block !important',
                         },
                         img: {
                             float: 'none',
